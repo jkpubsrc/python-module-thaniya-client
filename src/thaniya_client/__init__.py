563884dfe767d04db7c8f492c8dc868bbffaae0a
jkpubsrc <pubsrc@binary-overflow.de>
2020-03-11 16:18:42 +0100

    Added backup task for system installation of MediaWiki

--- src/thaniya_client/__init__.py
+++ src/thaniya_client/__init__.py
@@ -1,7 +1,7 @@
 ﻿
 
 
-__version__ = "0.2020.3.11.3"
+__version__ = "0.2020.3.11.4"
 
 
 
@@ -23,13 +23,13 @@ from .ThaniyaBackupDriver import ThaniyaBackupDriver
 
 from .ThaniyaTask_BackupDir import ThaniyaTask_BackupDir
 from .ThaniyaTask_BackupMediaWiki_User import ThaniyaTask_BackupMediaWiki_User
+from .ThaniyaTask_BackupMediaWiki import ThaniyaTask_BackupMediaWiki
 from .ThaniyaTask_BackupDevice import ThaniyaTask_BackupDevice
 from .ThaniyaTask_BackupTypo3 import ThaniyaTask_BackupTypo3
 
 from .AbstractTargetDirectoryStrategy import AbstractTargetDirectoryStrategy
 from .TargetDirectoryStrategy_StaticDir import TargetDirectoryStrategy_StaticDir
 from .TargetDirectoryStrategy_DateDefault import TargetDirectoryStrategy_DateDefault
-
 
 
 
